e353a8fd40f0c166630d06cd111499ce0d0ff680
shettyani-rudh <158299994+shettyani-rudh@users.noreply.github.com>
2024-10-03 16:59:40 +0530

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        
+        System.out.println("Choose an operation (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+        
+        double result = 0;
+        switch(operation) {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': 
+                if(num2 != 0) result = num1 / num2; 
+                else System.out.println("Division by zero is not allowed.");
+                break;
+            default: System.out.println("Invalid operation.");
+        }
+        
+        System.out.println("Result: " + result);
+    }
+}
